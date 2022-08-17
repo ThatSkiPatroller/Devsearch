d060f06ec141bc3ea19b1dc2159e1f477c596002
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2022-08-17 03:34:39 -0500

    Styled login and register page, currently having a problem with script tag not wanting to allow user to press x on error message for login page

--- pink/users/views.py
+++ pink/users/views.py
@@ -4,9 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
 # Create your views here.
 
 def loginUser(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -26,14 +28,36 @@ def loginUser(request):
             login(request, user)
             return redirect('profiles')
         else:
-            messages.error('Username or password is incorrect')
+            messages.error(request, 'Username or password is incorrect')
 
     return render(request, 'users/login_register.html')
 
 def logoutUser(request):
     logout(request)
-    messages.error(request, 'User was logged out!')
+    messages.info(request, 'User was logged out!')
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created!')
+
+            login(request, user)
+            return redirect('profiles')
+        
+        else:
+            messages.success(request, 'An error has occurred during registration')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 def profiles(request):
     profiles = Profile.objects.all()
